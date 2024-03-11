93f157f0a1f3f65e5bb02e675e1013cb9a6dbeb3
gitTanzj <kalle.riit@gmail.com>
2024-03-11 11:23:42 +0200

    kontrolleri koostamine ja selle töö kontrollimine

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response } from "express";
 import cors from 'cors';
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
 
 mongoose.connect(dotenv.config().parsed?.MONGO_URI as string);
 const database = mongoose.connection;
@@ -19,10 +20,14 @@ const app: Express = express();
 app.use(cors({
     origin: ['http://localhost:3000']
   }));
+app.use(bodyParser.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
+
+import articleController from './controllers/articleController';
+app.use('/article', articleController);
 
 app.listen(3006,() => {
   console.log(`[server]: Server is running at http://localhost:3006`);
